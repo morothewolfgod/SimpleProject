15228c6f90aa01639353c6038573dbe7042d00d1
Andrea Castillo <andreacastillo@MacBook-Pro.local>
2020-07-02 12:20:03 -0400

    Updated database from sqlite3 to Postgres

--- db/migrate/20200624022722_create_projects.rb
+++ db/migrate/20200624022722_create_projects.rb
@@ -1,7 +1,7 @@
 class CreateProjects < ActiveRecord::Migration[5.2]
   def change
     create_table :projects do |t|
-      t.string :name
+      t.text :name
       t.text :description
       t.references :user, foreign_key: true
 
